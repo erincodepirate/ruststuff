73925fb303ba7bb0f727e2c600a9ccaf7ce553ee
Erin Clark <erincodepirate@gmail.com>
2022-02-03 17:38:26 -0800

    Floats

---
+++ floats.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 1.1;
+    let y = 2.2;
+    println!("x times y is {}", x * y);
+
+    let mut z = 1.1;
+    println!("z is {}", z);
+    z = 3.3;
+    println!("z is {}", z);
+}
